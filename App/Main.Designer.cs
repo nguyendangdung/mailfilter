5408d9c3316deab749c3fb0f2c1a12d82d1f7fce
dan nguyen <ddnguyen0702@hotmail.com>
2017-02-24 15:02:56 +0700

    add App project

---
+++ App/Main.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace App
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.workingPage = new System.Windows.Forms.TabPage();
+            this.historyPage = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.workingPage);
+            this.tabControl1.Controls.Add(this.historyPage);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(638, 304);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // workingPage
+            // 
+            this.workingPage.Location = new System.Drawing.Point(4, 22);
+            this.workingPage.Name = "workingPage";
+            this.workingPage.Padding = new System.Windows.Forms.Padding(3);
+            this.workingPage.Size = new System.Drawing.Size(630, 278);
+            this.workingPage.TabIndex = 0;
+            this.workingPage.Text = "Working";
+            this.workingPage.UseVisualStyleBackColor = true;
+            // 
+            // historyPage
+            // 
+            this.historyPage.Location = new System.Drawing.Point(4, 22);
+            this.historyPage.Name = "historyPage";
+            this.historyPage.Padding = new System.Windows.Forms.Padding(3);
+            this.historyPage.Size = new System.Drawing.Size(630, 278);
+            this.historyPage.TabIndex = 1;
+            this.historyPage.Text = "History";
+            this.historyPage.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 328);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage workingPage;
+        private System.Windows.Forms.TabPage historyPage;
+    }
+}
+
---
+++ Services/Services/FileMailFilterService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Services;
+
+namespace Services.Services
+{
+    public class FileMailFilterService : IMailFilterService
+    {
+        public Task StartFilterAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
